f8b2c528c4a5f9a74242a66cf8ec638cc7ca49a9
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-10-07 18:39:25 -0600

    Finished this assignment

--- httpclient.py
+++ httpclient.py
@@ -21,6 +21,7 @@
 import sys
 import socket
 import re
+import ssl
 # you may use urllib to encode data appropriately
 import urllib.parse
 
@@ -33,20 +34,65 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self, url):
+        parsed_url = urllib.parse.urlparse(url)
+        hostname = parsed_url.hostname
+        port = parsed_url.port
+
+        if not port:
+            scheme = parsed_url.scheme
+            if scheme == "http":
+                port = 80
+            elif scheme == "https":
+                port = 443
+
+        return hostname, port
+    
+    def get_path(self, url):
+        parsed_url = urllib.parse.urlparse(url)
+        path = parsed_url.path
+
+        if not path:
+            path = '/'
+
+        return path
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # if HTTPs, create a socket for SSL/TLS communication
+        if port == 443:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            context.load_default_certs() # required for certificate verification
+            self.socket = context.wrap_socket(self.socket, server_hostname=host)
+
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        code = int(data.split(' ')[1])
+        return code
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        headers = {}
+        response_lines = data.split('\r\n')
+        for line in response_lines:
+            key_value = line.split(":", 1)
+
+            # If it's a key-value pair, then it's a header
+            if (len(key_value) == 2):
+                headers[key_value[0]] = key_value[1].strip()
+        
+        return headers
+
 
     def get_body(self, data):
+        body = re.search(r"\r\n\r\n(.*)", data, re.DOTALL)
+        
+        if body:
+            body = body.group(1) # return everything after \r\n\r\n
+            return body
+        
         return None
     
     def sendall(self, data):
@@ -65,17 +111,67 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode('latin-1')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        host, port = self.get_host_port(url)
+        path = self.get_path(url)
+        self.connect(host, port)
+
+        request = self.build_request("GET", host, path)
+
+        self.sendall(request)
+        response = self.recvall(self.socket)
+        self.socket.close()
+
+        response_code = self.get_code(response)
+        response_body = self.get_body(response)
+
+        print(response_code)
+        print(response_body)
+
+        # If a 3xx response code is returned, redirect to the new location
+        if 300 <= response_code < 400:
+            response_headers = self.get_headers(response)
+            location = response_headers["Location"]
+            self.GET(location)
+        
+        return HTTPResponse(response_code, response_body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        host, port = self.get_host_port(url)
+        path = self.get_path(url)
+        self.connect(host, port)
+
+        request = self.build_request("POST", host, path, args)
+        self.sendall(request)
+        response = self.recvall(self.socket)
+        print(response)
+        self.socket.close()
+
+        response_code = self.get_code(response)
+        response_body = self.get_body(response)
+        return HTTPResponse(response_code, response_body)
+    
+    def build_request(self, request_method, host, path, args=None):
+        request_headers = \
+                f"{request_method} {path} HTTP/1.1\r\n" + \
+                f"Host: {host}\r\n" + \
+                "Connection: close\r\n"
+        request_body = ""
+
+        if request_method == "POST":
+            if args:
+                request_body = '&'.join([f'{key}={value}' for key, value in args.items()])
+                
+                # Additional headers
+                request_headers += "Content-type: application/x-www-form-urlencoded\r\n"
+            
+            # Always include a Content Length with the POST 
+            # This is needed if we are POSTing headers without a request body
+            request_headers += f"Content-length: {len(request_body.encode('utf-8'))}\r\n"
+             
+        return request_headers + "\r\n" + request_body
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
